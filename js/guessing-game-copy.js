214e21785e9ef4f8c2951a986014ea1b014bb17c
Cileb <Cileb@users.noreply.github.com>
2022-07-14 19:16:39 -0400

    v1

---
+++ js/guessing-game-copy.js
@@ -0,0 +1,135 @@
+function generateWinningNumber() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+function shuffle(arr) {
+  arr.sort(function (a, b) {
+    return 0.5 - Math.random();
+  });
+  return arr;
+}
+function newGame() {
+  playersGuess = null;
+  game.attempts = 0;
+  pastGuesses = [];
+  titleText.textContent = "MY GUESSING GAME";
+  currentStatus.textContent = "Guess a number between 1-100";
+  game.winningNumber = generateWinningNumber();
+  document.getElementById("guess-box").value = null;
+  for (let attempt in guess) {
+    guess[attempt].textContent = "-";
+  }
+  submitGuess.disabled = false;
+  hintButton.disabled = false;
+}
+function playersGuessSubmission(number) {
+  if (number < 1 || number > 100 || typeof number !== "number") return false;
+  else return number;
+}
+function isLower(number) {
+  if (number < game.winningNumber) return true;
+  else return false;
+}
+function isHigher(number) {
+  if (number > game.winningNumber) return true;
+  else return false;
+}
+function checkGuess(number) {
+  if (pastGuesses.includes(number)) {
+    currentStatus.textContent = "You already guessed that.";
+  } else if (number === game.winningNumber) {
+    titleText.textContent = "You Win!";
+    currentStatus.textContent = "Congratulations!";
+    guess[game.attempts].textContent = number;
+    submitGuess.disabled = true;
+    hintButton.disabled = true;
+  } else if (isLower(number)) {
+    currentStatus.textContent = "Guess Higher!";
+    guess[game.attempts].textContent = number;
+    pastGuesses.push(number);
+    game.attempts += 1;
+  } else if (isHigher(number)) {
+    currentStatus.textContent = "Guess Lower!";
+    guess[game.attempts].textContent = number;
+    pastGuesses.push(number);
+    game.attempts += 1;
+  } else return false;
+  if (playersGuess) {
+    let difference = Math.abs(playersGuess - game.winningNumber);
+    if (difference) {
+      if (difference < 10) {
+        titleText.textContent = "You're burning up!";
+      } else if (difference < 25) {
+        titleText.textContent = "You're lukewarm.";
+      } else if (difference < 50) {
+        titleText.textContent = "You're a bit chilly.";
+      } else if (difference >= 50) {
+        titleText.textContent = "You're ice cold!";
+      }
+    }
+  }
+}
+function provideHint() {
+  let hint = shuffle([
+    game.winningNumber,
+    generateWinningNumber(),
+    generateWinningNumber(),
+  ]);
+  titleText.textContent = `The winning number is either ${hint[0]}, ${hint[1]}, or ${hint[2]}.`;
+}
+
+const hintButton = document.getElementById("hint");
+const titleText = document.getElementById("title");
+const playAgain = document.getElementById("play-again");
+const submitGuess = document.getElementById("guess-button");
+const currentStatus = document.getElementById("current-status");
+const guess = document.querySelectorAll(".guess");
+const guessBox = document.getElementById("guess-box").value;
+
+let game = {
+  attempts: 0,
+  winningNumber: generateWinningNumber(),
+};
+
+let pastGuesses = [];
+
+hintButton.addEventListener("click", function () {
+  provideHint();
+});
+
+playAgain.addEventListener("click", function () {
+  newGame();
+});
+
+submitGuess.addEventListener("click", function () {
+  playersGuess = playersGuessSubmission(
+    Number(document.getElementById("guess-box").value)
+  );
+
+  if (!playersGuess) {
+    currentStatus.textContent = "That is an invalid guess.";
+  } else {
+    checkGuess(playersGuess);
+  }
+  if (game.attempts >= 5) {
+    submitGuess.disabled = true;
+    hintButton.disabled = true;
+    titleText.textContent = `You Lose!`;
+    currentStatus.textContent = `Game Over - Try Again! The Winning Number was: ${game.winningNumber}`;
+  }
+});
+
+// else if (checkGuess(playersGuess) === "You Win!") {
+//   titleText.textContent = "You Win!";
+//   currentStatus.textContent = "Congratulations!";
+//   guess[attempts].textContent = playersGuess;
+//   submitGuess.disabled = true;
+// } else if (isLower(playersGuess)) {
+//   currentStatus.textContent = "Guess Higher!";
+//   guess[attempts].textContent = playersGuess;
+//   attempts += 1;
+// } else if (isHigher(playersGuess)) {
+//   currentStatus.textContent = "Guess Lower!";
+//   guess[attempts].textContent = playersGuess;
+//   attempts += 1;
+// }
+// Number(document.getElementById("guess-box").value)
--- js/guessing-game.js
+++ js/guessing-game.js
@@ -7,3 +7,71 @@ In this file, you will also include the event listeners that are needed to inter
 a user clicks a button or adds a guess to the input field.
 
 */
+let attempts = 0;
+let guessNumber = Math.floor(Math.random() * 100) + 1;
+const guessBox = document.getElementById("guess-box").value;
+const submitGuess = document.getElementById("guess-button");
+const playAgain = document.getElementById("play-again");
+const guess = document.querySelectorAll(".guess");
+const currentStatus = document.getElementById("current-status");
+
+const hintButton = document.getElementById("hint");
+console.log(guessNumber);
+
+hintButton.addEventListener("click", function () {
+  let hint = [
+    guessNumber,
+    Math.floor(Math.random() * 100 + 1),
+    Math.floor(Math.random() * 100 + 1),
+  ];
+  hint.sort(function (a, b) {
+    return 0.5 - Math.random();
+  });
+  if (attempts > 0) {
+    titleText.textContent = `The winning number is either ${hint[0]}, ${hint[1]}, or ${hint[2]}.`;
+  } else {
+    titleText.textContent = "At least try before I give you a hint!";
+  }
+});
+
+playAgain.addEventListener("click", function () {
+  location.reload();
+});
+
+submitGuess.addEventListener("click", function () {
+  let currentGuess = document.getElementById("guess-box").value;
+  if (currentGuess) {
+    if (Math.abs(currentGuess - guessNumber)) {
+      if (Math.abs(currentGuess - guessNumber) < 10) {
+        titleText.textContent = "You're burning up!";
+      } else if (Math.abs(currentGuess - guessNumber) < 25) {
+        titleText.textContent = "You're lukewarm.";
+      } else if (Math.abs(currentGuess - guessNumber) < 50) {
+        titleText.textContent = "You're a bit chilly.";
+      } else if (Math.abs(currentGuess - guessNumber) >= 50) {
+        titleText.textContent = "You're ice cold!";
+      }
+    }
+  }
+  if (currentGuess < 1 || currentGuess > 100) {
+    currentStatus.textContent = "That is an invalid guess.";
+  } else if (currentGuess > guessNumber) {
+    currentStatus.textContent = "Guess Lower!";
+    guess[attempts].textContent = currentGuess;
+    attempts += 1;
+  } else if (currentGuess < guessNumber) {
+    currentStatus.textContent = "Guess Higher!";
+    guess[attempts].textContent = currentGuess;
+    attempts += 1;
+  } else if (currentGuess == guessNumber) {
+    titleText.textContent = `You Win! The winning number was ${guessNumber}.`;
+    currentStatus.textContent = `PLAY AGAIN?`;
+    guess[attempts].textContent = currentGuess;
+    submitGuess.disabled = true;
+  }
+  if (attempts >= 5) {
+    titleText.textContent = `You Lose!`;
+    currentStatus.textContent = "Game Over - Try Again!";
+    submitGuess.disabled = true;
+  }
+});
